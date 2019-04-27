07ab7e00e48937252762e308681169616a246f3c
James <james.mt13@gmail.com>
2019-04-27 21:12:42 +0100

    update for Sherlock 0.5

--- src/main/java/uk/ac/warwick/dcs/sherlock/module/model/extra/ModuleModelExtra.java
+++ src/main/java/uk/ac/warwick/dcs/sherlock/module/model/extra/ModuleModelExtra.java
@@ -1,10 +1,10 @@
 package uk.ac.warwick.dcs.sherlock.module.model.extra;
 
-import uk.ac.warwick.dcs.sherlock.api.SherlockRegistry;
 import uk.ac.warwick.dcs.sherlock.api.annotation.EventHandler;
 import uk.ac.warwick.dcs.sherlock.api.annotation.SherlockModule;
 import uk.ac.warwick.dcs.sherlock.api.event.EventInitialisation;
 import uk.ac.warwick.dcs.sherlock.api.event.EventPreInitialisation;
+import uk.ac.warwick.dcs.sherlock.api.registry.SherlockRegistry;
 import uk.ac.warwick.dcs.sherlock.module.model.base.lang.HaskellLexer;
 
 @SherlockModule
@@ -12,12 +12,12 @@ public class ModuleModelExtra {
 
 	@EventHandler
 	public void preInitialisation(EventPreInitialisation event) {
-		SherlockRegistry.registerLanguage("Haskell", HaskellLexer.class); //Testing, will be moved to a separate module for testing purposes
+		SherlockRegistry.registerLanguage("Haskell", HaskellLexer.class);
 	}
 
 	@EventHandler
 	public void initialisation(EventInitialisation event) {
-		SherlockRegistry.registerDetector(TestDetector2.class);
+		SherlockRegistry.registerDetector(ModuleExampleDetector.class);
 	}
 
 }
